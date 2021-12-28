571e5dc12ab21837778a2fbfa0562d939b525766
Patri <patri@malvidopazo.es>
2021-12-28 17:03:01 +0100

    add event and function js

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,43 @@
 'use strict';
 
 console.log('>> Ready :)');
+
+const animeList = document.querySelector('.js-anime_list');
+const searchInputTitle = document.querySelector('.js-search_input_title');
+const buttonSearch = document.querySelector('.js-button_search');
+
+let data = [];
+let dataList = [];
+
+fetch(`https://api.jikan.moe/v3/search/anime?q=${searchInputTitle.value}`)
+  .then((response) => response.json())
+  .then((dataFromApi) => {
+    data = dataFromApi.results;
+    console.log(data);
+    renderAllTitles();
+  });
+
+function renderAllTitles(){
+    animeList.innerHTML = '';
+    for (const eachAnime of data){
+    renderTitle(eachAnime);
+}
+}
+
+
+function renderTitle(data) {
+ 
+  animeList.innerHTML += `
+<article class="movie">
+    <img class="" src="${data.image_url}" alt="">
+    <h3 class="card_title">${data.title}</h3>
+</article>`;
+}
+
+function handleShowTitle(event){
+    const selectedAnimeTitle = event.currentTarget.dataset.title;
+    dataList.push(selectedAnimeTitle);
+
+}
+
+buttonSearch.addEventListener('click', handleShowTitle);
