10cfb10d8c69ca482b01e7a8404f919ebb08fc30
Patri <patri@malvidopazo.es>
2021-12-28 17:22:06 +0100

    button event and show data api ready

--- src/js/main.js
+++ src/js/main.js
@@ -1,32 +1,30 @@
-'use strict';
+"use strict";
 
-console.log('>> Ready :)');
+console.log(">> Ready :)");
 
-const animeList = document.querySelector('.js-anime_list');
-const searchInputTitle = document.querySelector('.js-search_input_title');
-const buttonSearch = document.querySelector('.js-button_search');
+const animeList = document.querySelector(".js-anime_list");
+const searchInputTitle = document.querySelector(".js-search_input_title");
+const buttonSearch = document.querySelector(".js-button_search");
 
 let data = [];
-let dataList = [];
 
-fetch(`https://api.jikan.moe/v3/search/anime?q=${searchInputTitle.value}`)
-  .then((response) => response.json())
-  .then((dataFromApi) => {
-    data = dataFromApi.results;
-    console.log(data);
-    renderAllTitles();
-  });
-
-function renderAllTitles(){
-    animeList.innerHTML = '';
-    for (const eachAnime of data){
-    renderTitle(eachAnime);
+function fetchItems() {
+  fetch(`https://api.jikan.moe/v3/search/anime?q=${searchInputTitle.value}`)
+    .then((response) => response.json())
+    .then((dataFromApi) => {
+      data = dataFromApi.results;
+      console.log(data);
+      renderAllItems();
+    });
 }
+function renderAllItems() {
+  animeList.innerHTML = "";
+  for (const eachAnime of data) {
+    renderItem(eachAnime);
+  }
 }
 
-
-function renderTitle(data) {
- 
+function renderItem(data) {
   animeList.innerHTML += `
 <article class="movie">
     <img class="" src="${data.image_url}" alt="">
@@ -34,10 +32,10 @@ function renderTitle(data) {
 </article>`;
 }
 
-function handleShowTitle(event){
-    const selectedAnimeTitle = event.currentTarget.dataset.title;
-    dataList.push(selectedAnimeTitle);
+function handleShowTitle(event) {
+    event.preventDefault();
+    fetchItems();
 
 }
 
-buttonSearch.addEventListener('click', handleShowTitle);
+buttonSearch.addEventListener("click", handleShowTitle);
