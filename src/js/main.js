f9919c97e71065d8df7f5b20f77aa08e1da3f421
Patri <patri@malvidopazo.es>
2022-01-02 21:00:42 +0100

    check if it is added to favorites

--- src/js/main.js
+++ src/js/main.js
@@ -1,18 +1,19 @@
 'use strict';
-
+//variables globales-----------------------------------
 console.log('>> Ready :)');
 const animeListFav = document.querySelector('.js-anime_list_fav');
 const animeList = document.querySelector('.js-anime_list');
 const searchInputTitle = document.querySelector('.js-search_input_title');
 const buttonSearch = document.querySelector('.js-button_search');
 const buttonReset = document.querySelector('.js-button_reset');
-const buttonResetFav = document.querySelector ('.js-button_reset_fav');
+const buttonResetFav = document.querySelector('.js-button_reset_fav');
 const defaultImage =
   'https://via.placeholder.com/210x295/ffffff/666666/?text=TV';
 
 let data = [];
 let dataFav = [];
 
+//Funcion para recoger datos de la APi
 function fetchItems() {
   fetch(`https://api.jikan.moe/v3/search/anime?q=${searchInputTitle.value}`)
     .then((response) => response.json())
@@ -22,6 +23,8 @@ function fetchItems() {
       renderAllItems();
     });
 }
+
+
 
 function renderAllItems() {
   animeList.innerHTML = '';
@@ -35,10 +38,20 @@ function renderAllItems() {
   }
 }
 
+
 function handleAnimeFav(event) {
-  dataFav.push(event.currentTarget.dataset);
-  setInLocalStorage();
-  renderAllItemsFav();
+  const existsFav = dataFav.find(
+    (fav) => fav.title === event.currentTarget.dataset.title
+  );
+
+  if (existsFav === undefined) {
+    dataFav.push(event.currentTarget.dataset);
+
+    setInLocalStorage();
+    renderAllItemsFav();
+  } else {
+    alert('La serie de anime ya está en la lista de favoritos');
+  }
 }
 
 function renderAllItemsFav() {
@@ -56,6 +69,7 @@ function renderItemFav(dataFav) {
   animeListFav.appendChild(li);
 }
 
+// Funcion para mostrar la lista de series de anime
 function renderItem(data) {
   let imageUrl = data.image_url;
 
@@ -71,6 +85,15 @@ function renderItem(data) {
   article.innerHTML = `<img class="" src="${imageUrl}" alt="" placeholder="">
   <h3 class="card__title">${data.title}</h3>`;
 
+
+  const isFav = dataFav.find(
+    (fav) => fav.title === data.title
+  );
+  
+  if (isFav !== undefined) {
+    article.classList.add('js-fav');
+  }
+
   animeList.appendChild(article);
 }
 
@@ -83,10 +106,13 @@ function handleClickReset() {
   location.reload();
 }
 
-
+function handleClickResetAllFav() {
+  dataFav = [];
+  localStorage.clear();
+  location.reload();
+}
 
 function setInLocalStorage() {
- 
   localStorage.setItem('dataLocalStorageFav', JSON.stringify(dataFav));
 }
 
@@ -103,4 +129,4 @@ getFromLocalStorage();
 
 buttonSearch.addEventListener('click', handleShowTitle);
 buttonReset.addEventListener('click', handleClickReset);
-buttonResetFav.addEventListener ('click',handleClickResetFav);
+buttonResetFav.addEventListener('click', handleClickResetAllFav);
